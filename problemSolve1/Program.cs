5a9054a6e9c011a209e79951d3f6ccda8e2505f1
sadiaafrin1529 <sadiaafrin1529@gmail.com>
2023-10-04 23:55:14 +0600

    Add project files.

---
+++ problemSolve1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemSolve1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Alexandra Abramov";
+            Console.WriteLine("Hello:" + name );
+
+            Console.Write("first Number:");
+            int numberOne =Convert.ToInt32 (Console.ReadLine());
+            Console.Write("first Number:");
+            double numberTwo= Convert.ToDouble(Console.ReadLine());
+            double sum = numberOne + numberTwo;
+            Console.WriteLine("Sum: " + sum);
+
+
+            Console.ReadKey();
+
+
+        }
+        
+    }
+}
